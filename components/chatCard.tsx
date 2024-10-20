4fd74d0fd0b0fe8feaba6de1a60db70f0c9eb013
TahirReyaz <tahirreyaz5@gmail.com>
2024-10-20 13:04:30 +0530

    Added chat menu

--- components/chatCard.tsx
+++ components/chatCard.tsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
+import { Ellipsis } from "lucide-react";
+
 import { SheetClose } from "./ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 interface Props {
   title: string;
@@ -8,14 +17,24 @@ interface Props {
 
 const ChatCard = ({ title, id }: Props) => {
   return (
-    <SheetClose asChild className="w-full">
-      <Link
-        href={`/chat/${id}`}
-        className="rounded hover:bg-santasGray/20 p-2 w-full flex"
-      >
-        {title}
-      </Link>
-    </SheetClose>
+    <div className="grid grid-cols-12 rounded hover:bg-santasGray/20">
+      <SheetClose asChild className="w-full col-span-10">
+        <Link href={`/chat/${id}`} className="p-2 w-full flex">
+          {title}
+        </Link>
+      </SheetClose>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="col-span-2 flex items-center cursor-pointer">
+            <Ellipsis />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem className="cursor-pointer">Rename</DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer">Delete</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 
