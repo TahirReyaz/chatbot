abe1cfcb5370e1a8d2ae8a787a3a0cd19006571d
TahirReyaz <tahirreyaz5@gmail.com>
2024-10-21 12:17:50 +0530

    Added meta title

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/auth";
 import LoginForm from "@/components/LoginForm";
 import { Session } from "../lib/definitions";
+
+export const metadata: Metadata = {
+  title: "LLM Connect · Login",
+  description: "Chatgpt-like AI Chatbot",
+};
 
 const Login = async () => {
   const session = (await auth()) as Session;
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/auth";
 import { Session } from "../lib/definitions";
 import SignupForm from "@/components/SignupForm";
+
+export const metadata: Metadata = {
+  title: "LLM Connect · Signup",
+  description: "Chatgpt-like AI Chatbot",
+};
 
 const Signup = async () => {
   const session = (await auth()) as Session;
