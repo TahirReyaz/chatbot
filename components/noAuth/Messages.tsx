d5c04d9e56e3243b3f8c49f69d689858ef75bdec
TahirReyaz <tahirreyaz5@gmail.com>
2024-10-21 17:56:43 +0530

    Added scroll to bottom in no auth messages

--- components/noAuth/Messages.tsx
+++ components/noAuth/Messages.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import { Message as MessageType } from "@/lib/definitions";
 import Message from "../Message";
 
@@ -6,12 +8,21 @@ interface Props {
 }
 
 const Messages = ({ messages }: Props) => {
+  const divRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (divRef?.current) {
+      divRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <div className="text-gray-50 max-h-[70vh] overflow-y-auto">
       {/* Render your messages here */}
       {messages?.map((message, index) => (
         <Message key={index} {...{ ...message }} />
       ))}
+      <div ref={divRef} />
     </div>
   );
 };
